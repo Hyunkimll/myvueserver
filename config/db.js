d728c2ce0565186439fc69d36dfa0d2d9c63861b
qingtian1210 <2497999352@qq.com>
2024-02-16 21:16:20 +0800

    Update .gitignore

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 let MYSQL_CONF={
-    host: 'localhost', //主机名
+   host: 'localhost', //主机名
+   port: "3306",
    user: 'root',//用户名
-   password: '123456',//密码
+   password: 'qingtian@240208',//密码
 //    port: 0000, //端口号
-   database: 'myvue'//数据库名称
+   database: 'lilu'//数据库名称
 }
 //暴露
 module.exports = {
