f53123ff8da7cd30ed6642bbcf530c05af81de11
qingtian1210 <2497999352@qq.com>
2024-02-16 21:28:11 +0800

    修改

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 let MYSQL_CONF={
    host: 'localhost', //主机名
    port: "3306",
-   user: 'root',//用户名
-   password: 'qingtian@240208',//密码
+  user: 'root',//用户名
+  password: '123456',//密码
 //    port: 0000, //端口号
-   database: 'lilu'//数据库名称
+  database: 'myvue'//数据库名称
 }
-//暴露
-module.exports = {
-MYSQL_CONF
-}
+ //暴露
+ module.exports = {
+ MYSQL_CONF
+ }
